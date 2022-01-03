f66d7a0eee6b9fe9bea1b1a93158a91b7921e171
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-01-03 17:35:51 +0100

    Initial commit. First lectures and code.

---
+++ exercises/src/main/java/org/pgr112/ex2/Assignment.java
@@ -0,0 +1,111 @@
+package org.pgr112.ex2;
+
+public class Assignment {
+    /**
+     * Returns the sum of all the received numbers.
+     */
+    public int addThreeNumbers(int i, int j, int k) {
+        return 0;
+    }
+
+    /**
+     * Evaluates if the received number is
+     * Small (less than 100).
+     * Big (greater than 1000).
+     * Medium (not small or big)
+     */
+    public String isNumberSmallMediumOrBig(int number){
+        return null;
+    }
+
+    /**
+     * Prints all Strings in received array to standard output.
+     * One String on each line.
+     */
+    public void printAllStrings(String[] strings){
+
+    }
+
+    /**
+     * Returns the sum of all numbers in received array.
+     */
+    public int arraySum(int[] numbers){
+        return 0;
+    }
+
+    /**
+     * Print all characters until a char is "."
+     * Do not print in separate lines.
+     */
+    public void printFirstSentence(char[] chars) {
+
+    }
+
+    /**
+     * Prints course name for provided course code.
+     * PGR103 -> Objektorientert programmering
+     * DB1102 -> Database
+     * TK2100 -> Informasjonssikkerhet
+     * or "Unknown" if none of the above.
+     */
+    public void printCourseName(String courseCode){
+
+    }
+
+    /**
+     * Prints all Strings in received array to standard output.
+     * One String on each line.
+     * But only if the String is not exactly "Corona".
+     */
+    public void printAllStringsNotCorona(String[] strings){
+
+    }
+
+    /**
+     * Prints the provided strings in upper case letters.
+     * One String on each line.
+     */
+    public void printUpperCaseStrings(String[] strings){
+
+    }
+
+    /**
+     * Returns true if provided color is represented in the Norwegian flag.
+     * Color input is lowercase only.
+     */
+    public boolean isColorInNorwegianFlag(String color){
+        return false;
+    }
+
+    /**
+     * Returns the index of the first occurrence of char c in String string.
+     * Returns -1 if char is not found.
+     */
+    public int firstOccurrence(String string, char c){
+        return 0;
+    }
+
+    /**
+     * Returns the combined length of the provided Strings.
+     */
+    public int combinedLength(String s1, String s2){
+        return 0;
+    }
+
+    /**
+     * Returns the sum of all the received numbers.
+     */
+    public int addNumbers(int... numbers){
+        return 0;
+    }
+
+    /**
+     * Prints all Strings in received array to standard output.
+     * One String on each line.
+     * But only if the String is not Corona (case insensitive).
+     */
+    public void printAllStringsNotCoronaCaseInsensitive(String[] strings){
+
+    }
+
+}
